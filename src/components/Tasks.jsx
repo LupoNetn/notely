eefb9fbcaf966f37234f877632b8951bba454305
oluwatobi <luponet07@gmail.com>
2025-08-29 06:38:50 -0400

    ..

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Ellipsis, Plus } from "lucide-react";
 
 const taskHeader = [
   { title: "BackLog" },
@@ -18,33 +19,68 @@ const Tasks = () => {
 
       <main>
         {/* Task Header */}
-
-        <div className="grid grid-cols-4 gap-4 my-4">
-
-          <div>
+        <div
+          className="
+            flex gap-4 overflow-x-auto
+            md:grid md:grid-cols-2
+            lg:grid lg:grid-cols-4
+          "
+        >
+          <div className="flex-1 min-w-[220px]">
             <div className="flex bg-background items-center justify-between p-4 rounded-md">
               <h3>BackLog</h3>
+              <div className="flex gap-2">
+                <button className="p-1 rounded-full bg-blue-200 hover:bg-blue-400 text-gray-600 hover:text-white cursor-pointer">
+                  <Plus size={16} />
+                </button>
+                <button className="p-1 cursor-pointer rounded-full hover:bg-primary/50">
+                  <Ellipsis size={16} />
+                </button>
+              </div>
             </div>
           </div>
 
-          <div>
+          <div className="flex-1 min-w-[220px]">
             <div className="flex bg-background items-center justify-between p-4 rounded-md">
               <h3>To Do</h3>
+              <div className="flex gap-2">
+                <button className="p-1 rounded-full bg-blue-200 hover:bg-blue-400 text-gray-600 hover:text-white cursor-pointer">
+                  <Plus size={16} />
+                </button>
+                <button className="p-1 cursor-pointer rounded-full hover:bg-primary/50">
+                  <Ellipsis size={16} />
+                </button>
+              </div>
             </div>
           </div>
 
-          <div>
+          <div className="flex-1 min-w-[220px]">
             <div className="flex bg-background items-center justify-between p-4 rounded-md">
               <h3>In Progress</h3>
+              <div className="flex gap-2">
+                <button className="p-1 rounded-full bg-blue-200 hover:bg-blue-400 text-gray-600 hover:text-white cursor-pointer">
+                  <Plus size={16} />
+                </button>
+                <button className="p-1 cursor-pointer rounded-full hover:bg-primary/50">
+                  <Ellipsis size={16} />
+                </button>
+              </div>
             </div>
           </div>
 
-          <div>
+          <div className="flex-1 min-w-[220px]">
             <div className="flex bg-background items-center justify-between p-4 rounded-md">
               <h3>Review</h3>
+              <div className="flex gap-2">
+                <button className="p-1 rounded-full bg-blue-200 hover:bg-blue-400 text-gray-600 hover:text-white cursor-pointer">
+                  <Plus size={16} />
+                </button>
+                <button className="p-1 cursor-pointer rounded-full hover:bg-primary/50">
+                  <Ellipsis size={16} />
+                </button>
+              </div>
             </div>
           </div>
-
         </div>
       </main>
     </>
