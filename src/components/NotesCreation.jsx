7ef3b01ff3500633cb8b2e2eaa0b51864384cf2e
oluwatobi <luponet07@gmail.com>
2025-08-30 16:55:37 -0400

    uninstalled react quill and added note creation page

--- src/components/NotesCreation.jsx
+++ src/components/NotesCreation.jsx
@@ -1,32 +1,48 @@
-import React from "react";
+import { ArrowLeft } from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 const NotesCreation = () => {
+  const [title,setTitle] = useState('');
+  const [content,setContent] = useState('');
+
   return (
-    <div className="flex justify-center items-start min-h-screen p-6">
-      <div className="  border border-gray-200 rounded-xl w-full max-w-2xl p-6">
+    <div className="flex items-start min-h-screen p-3 ">
+      {/* Main container aligned left */}
+      <div className="w-full p-3">
         {/* Header */}
-        <h1 className="text-2xl font-semibold text-gray-800 mb-4">
+        <div className="flex items-center mb-6 gap-3">
+          <Link to='/notes'>
+           <div>
+            <ArrowLeft className="w-8 h-8 -mt-3"/>
+          </div>
+          </Link>
+          <h1 className="text-2xl font-semibold text-gray-800 mb-4">
           Create Note
         </h1>
-
+        </div>
         {/* Title Input */}
         <input
           type="text"
           placeholder="Title"
-          className="w-full text-lg font-medium border-b border-transparent outline-none mb-4 p-2 rounded bg-gray-50 
-            focus:bg-white focus:border-b-blue-400 transition"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full max-w-[500px] text-lg font-medium border-gray-200  focus:border-blue-200 outline-none mb-4 p-3 rounded transition"
         />
 
         {/* Note Area */}
         <textarea
           placeholder="Take a note..."
-          className="w-full min-h-[200px] p-3 rounded-md bg-gray-50 text-gray-700 outline-none resize-none
-            border-b border-transparent focus:bg-white focus:border-b-blue-400 transition"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="w-full min-h-[300px] p-4 rounded-md  text-gray-700 outline-none resize-none
+            border border-gray-200  focus:border-blue-200 transition"
         />
 
         {/* Save Button */}
         <div className="mt-6 flex justify-end">
-          <button className="bg-blue-600 hover:bg-blue-300 text-primary px-6 py-2 rounded-full font-medium shadow-sm transition">
+          <button className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-2 rounded-full font-medium shadow-md transition">
             Save
           </button>
         </div>
